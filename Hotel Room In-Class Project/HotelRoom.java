d578f47aab48dc1a9e7157cf8e4a232357d644c6
Engineering <Engineering@308-16.local>
2022-09-12 11:38:41 -0400

    Final; No Extra Credit

--- Hotel Room In-Class Project/HotelRoom.java
+++ Hotel Room In-Class Project/HotelRoom.java
@@ -1,66 +1,66 @@
-public class HotelRoom{
+public class HotelRoom{ //creates HotelClass and 4 variables
     int roomNumber;
     Person guestOne;
     Person guestTwo;
     int c = 0;
-HotelRoom(int rn){
+
+    HotelRoom(int rn){ //hotel method constructor 
     roomNumber = rn;
     
 }
 
-void checkIn(Person guest){
-    if(guestOne == null){
+void checkIn(Person guest){ //check in method constructor
+    if(guestOne == null){ // assigns guest to guestOne if guestOne has not been assigned yet 
         guestOne = guest;
     
     }
-    else if(guestOne != null && guestTwo == null){
+    else if(guestOne != null && guestTwo == null){ //assigns guest to guestTwo if guestOne is full and guestTwo is null
         guestTwo = guest;
 
     }
-    else if(guestOne != null && guestTwo != null){
+    else if(guestOne != null && guestTwo != null){ //if both guests are full, prints romm capacity warning
         System.out.println("There is no space in this room!");
     }
  
-    System.out.println(guest.name + " checked into room number " + roomNumber +"!");
+    System.out.println(guest.name + " checked into room number " + roomNumber +"!"); //prints out name of guest, if check in is succesful
     }
 
 
 
 
-void checkOut(Person guest){
-    if(guest == guestOne){
+void checkOut(Person guest){ //constructor for check out methodf
+    if(guest == guestOne){ //checks out guestOne if guestOne is assigned a guest
         guestOne = null;
-        System.out.println(guest.name + " has checked out of room number " + roomNumber + "!" );
+        System.out.println(guest.name + " has checked out of room number " + roomNumber + "!" );// prints out check out confirmation if check out is successful
     }
-    else if (guest == guestTwo){
+    else if (guest == guestTwo){ //checks out guestTwo if guestTwo is assigned a guest
         guestTwo = null; 
-        System.out.println(guest.name + " has checked out of room number " + roomNumber + "!" );
+        System.out.println(guest.name + " has checked out of room number " + roomNumber + "!" ); // prints out check out confirmation if check out is successful
     }
 }
 
 
-int checkCapacity(){
-     int c = 0;
-    if(guestOne != null){
+int checkCapacity(){ //check capacity constructor 
+     int c = 0; //creats variable c
+    if(guestOne != null){ //if guestOne is assigned, add one to capacity
         c++;
     
     }
 
-    if(guestTwo != null){
+    if(guestTwo != null){ //if guestTwo is assigned, add one to capacity
         c++;
     }
-
-    return(c);
+    return(c); //returns capacity value
 }
 
 
 
-void printGuestInfo(){
-    if(guestOne != null){
+void printGuestInfo(){ //print guest info constructor
+    if(guestOne != null){ //prints guest info if guestOne is assigned 
         System.out.println(guestOne.name + " is a resident at the hotel. He is  " + guestOne.age + " years old and his hair color is " + guestOne.hairColor);
 
     }
-    if(guestTwo != null){
+    if(guestTwo != null){ //prints guest info if guestTwo is assigned 
         System.out.println(guestTwo.name + " is a resident at the hotel. He is " + guestTwo.age + " years old and his hair color is " + guestTwo.hairColor);
     }
 
--- Hotel Room In-Class Project/test.java
+++ Hotel Room In-Class Project/test.java
@@ -1,18 +1,6 @@
-class test{
- public static void main(String[] args) {
-    int c = 0;
-    Person Gabe = new Person(6.01, 16, "Gabe", "Brown");
-    Person George = new Person(5.5, 16, "George", "Dirty Blonde");
-
-    HotelRoom Marriot = new HotelRoom(1);
-    Marriot.checkIn(Gabe);
-    Marriot.checkIn(George);
-
-    Marriot.checkOut(Gabe);
-
-    c = Marriot.checkCapacity();
-    System.out.println(c);
-
-    Marriot.printGuestInfo();
+public class Test{
+    public static void main(String args[]){
+        new hotelTests();
+        
     }
 }
